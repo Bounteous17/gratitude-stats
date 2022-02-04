f4f3c68de58badeecbf777f5675180f17b788ad8
Àlex Serra <bounteous17@protonmail.com>
2022-02-04 12:29:13 +0100

    test basic root endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,8 +17,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return basic message', () => {
+      expect(appController.getRoot()).toBe('Up and running ...');
     });
   });
 });
