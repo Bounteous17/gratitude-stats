5c5b6a53b1d79269794590cc0e428b335eef6903
Àlex Serra <bounteous17@protonmail.com>
2022-02-04 12:38:45 +0100

    dynamic reading env vars from main function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getRoot(): string {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(app.get<ConfigService>(ConfigService).get('HTTP_PORT'));
 }
 bootstrap();
